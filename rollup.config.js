7e4987e618c0bc1bd96429b643b2fe66f56c698a
bwhite03 <bwhite1195@yahoo.com>
2022-03-15 11:41:54 -0400

    add react, react-dom to rollup.config

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,26 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
-import postcss from "rollup-plugin-postcss";
-import { terser } from "rollup-plugin-terser";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 //import scss from "rollup-plugin-scss";
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json');
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
         file: packageJson.main,
-        format: "cjs",
+        format: 'cjs',
         sourcemap: true,
       },
       {
         file: packageJson.module,
-        format: "esm",
+        format: 'esm',
         sourcemap: true,
       },
     ],
@@ -29,8 +29,8 @@ export default [
       resolve(),
       commonjs(),
       typescript({
-        tsconfig: "./tsconfig.json",
-        exclude: [/.stories.((js|jsx|ts|tsx|mdx))$/, "support"],
+        tsconfig: './tsconfig.json',
+        exclude: [/.stories.((js|jsx|ts|tsx|mdx))$/, 'support'],
       }),
       postcss(),
       // scss({
@@ -41,10 +41,11 @@ export default [
       // }),
       terser(),
     ],
+    external: ['react', 'react-dom'],
   },
   {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
     external: [/\.css$/],
   },
